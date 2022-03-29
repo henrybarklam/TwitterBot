158065f4766c0070cfcbaa27605f9003979213a9
Henry Barklam <henrybarklam@gmail.com>
2022-03-29 19:50:51 -0400

    Keys added

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -1,1 +1,12 @@
+import tweepy
+
 print("this is my twitter bot")
+
+CONSUMER_KEY = 'qONkRYulgiGT3go3uKlhu0l7a'
+CONSUMER_SECRET = 'SlVMrASGJiRuUok6To4NoFEWaxAHxDgcYONhHpX3doav9nYeSD'
+ACCESS_KEY = '1508946544862670855-FG2FtgH6lOg7zTtIeYwVgzgWxJ30I9'
+ACCESS_SECRET = 'zjvXUApw3fzkqa2DvurghIAhCULHrLAwbCfw6Ln0Wvl3z'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
